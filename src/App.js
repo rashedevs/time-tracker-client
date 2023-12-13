f50b469061a91c4f71cbae1a57f5d95601dc7b12
rashedevs <rasheduap2015@gmail.com>
2023-12-14 02:02:53 +0600

    setup router for app along with all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Entries from "./Components/Entries/Entries";
+import WeeklySheet from "./Components/WeeklySheet/WeeklySheet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello from client</header>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/entries" element={<Entries />} />
+        <Route path="/weeklysheet" element={<WeeklySheet />} />
+      </Routes>
     </div>
   );
 }
