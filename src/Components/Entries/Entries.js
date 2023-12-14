6fc7a4224e1fb139045d793be20319b40d904da8
rashedevs <rasheduap2015@gmail.com>
2023-12-14 17:17:26 +0600

    fetching all entries to entries page

--- src/Components/Entries/Entries.js
+++ src/Components/Entries/Entries.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Entries.css";
 
 const Entries = () => {
+  const [formDataArray, setFormDataArray] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8800/entries");
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setFormDataArray(data);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <p>Entries</p>
+    <div className="card-container">
+      {formDataArray.length > 0 ? (
+        formDataArray.map((formData, index) => (
+          <div key={index} className="card">
+            <p>
+              <strong>Start Time:</strong> {formData.start_time}
+            </p>
+            <p>
+              <strong>End Time:</strong> {formData.end_time}
+            </p>
+            <p>
+              <strong>Date:</strong> {formData.date}
+            </p>
+            <p>
+              <strong>Notes:</strong> {formData.notes}
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
     </div>
   );
 };
